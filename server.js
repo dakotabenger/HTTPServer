c604cc12af1218e2c9450c634ca235cfee2efee0
root <root@DESKTOP-LE7491G.localdomain>
2020-08-17 14:29:37 -0700

    Finished Bonus

--- server.js
+++ server.js
@@ -3,46 +3,51 @@ const http = require("http");
 http
   .createServer((req, res) => {
     if (req.url === "/OK") {
-        console.log(" 'Ok' request being processed...")
-        res.writeHead(200);
-        res.end();
+      console.log(" 'Ok' request being processed...");
+      res.writeHead(200, { "Content-type": "text/html" });
+      res.write(`<h1> Welcone to our site!</h1>`);
+      res.end();
     } else if (req.url === "/Bad-Request") {
-        console.log("This is a bad request")
-        res.writeHead(400);
-        res.end();
+      console.log("This is a bad request");
+      res.writeHead(400);
+      res.end();
     } else if (req.url === "/Created") {
-        console.log("Created")
+      if (req.method === "POST") {
+        console.log("Created");
         res.writeHead(201);
         res.end();
+      } else {
+        res.writeHead(405);
+        res.end();
+      }
     } else if (req.url === "/Forbidden") {
-        console.log("Access is forbidden");
-        res.writeHead(403);
-        res.end();
+      console.log("Access is forbidden");
+      res.writeHead(403);
+      res.end();
     } else if (req.url === "/Found") {
-        console.log("Found")
-        res.writeHead(302);
-        res.end();
+      console.log("Found");
+      res.writeHead(302, { Location: "/OK" });
+      res.end();
     } else if (req.url === "/Gateway-Timeout") {
-        console.log('Gateway Timeout');
-        res.writeHead(504);
-        res.end();
+      console.log("Gateway Timeout");
+      res.writeHead(504);
+      res.end();
     } else if (req.url === "/Internal-Server-Error") {
-        console.log('Internal server error')
-        res.writeHead(500);
-        res.end();
+      console.log("Internal server error");
+      res.writeHead(500);
+      res.end();
     } else if (req.url === "/Moved-Permanently") {
-        console.log("Moved Permanently"); 
-        res.writeHead(301);
-        res.end();
+      console.log("Moved Permanently", { Location: "/OK" });
+      res.writeHead(301);
+      res.end();
     } else if (req.url === "/Unauthorized") {
-        console.log('Unauthorized');
-        res.writeHead(401);
-        res.end();
-    }
-    else {
-        console.log("404 Not Found")
-        res.writeHead(404);
-        res.end();
+      console.log("Unauthorized");
+      res.writeHead(401);
+      res.end();
+    } else {
+      console.log("404 Not Found");
+      res.writeHead(404);
+      res.end();
     }
   })
   .listen(3000, function () {
